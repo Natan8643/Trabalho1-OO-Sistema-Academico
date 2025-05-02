90fbd6f7f14ce5367f069d9540b0166423679627
Natan França <natan.j.franca@gmail.com>
2025-05-02 19:20:19 -0300

    criando os atributos e os metodos de alunos

--- src/Aluno.java
+++ src/Aluno.java
@@ -1,9 +1,37 @@
 public class Aluno {
-    private String Nome;
-    private Integer Matricula;
-    private String Curso;
+    private String nome;
+    private Integer matricula;
+    private String curso;
 
     public Aluno(){
-        
+        this.nome = "";
+        this.matricula = 0;
+        this.curso = "";
+    }
+
+    public String getNome(){
+        return nome; 
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public Integer getMatricula(){
+        return matricula;
+    }
+
+    public void setMatricula(int matricula){
+        this.matricula = matricula;
+    }
+
+    public String getCurso(){
+        return curso;
+    }
+
+    public void setCurso(String curso){
+        this.curso = curso;
     }
 }
+
+
