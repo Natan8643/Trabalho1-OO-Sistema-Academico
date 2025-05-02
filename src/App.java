2583686350bf91d081866f31659e9e10fa343aac
Natan França <natan.j.franca@gmail.com>
2025-05-02 15:41:35 -0300

    criando funções no menu

--- src/App.java
+++ src/App.java
@@ -1,17 +1,46 @@
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class App {
+
     public static void main(String[] args) throws Exception {
         System.out.println("\n---------------------------------------");
         System.out.println("Olá usuário! O que gostaria de fazer?");
         System.out.println("---------------------------------------");
-        System.out.println("Digite o número de acordo com sua escolha:\n");
-        System.out.println("1 - Modo Aluno\n2 - Modo Disciplina\n3 - Modo Acompanhamento\n");
+        menuInicial();
+    }
+
+    public static void menuInicial() {
 
         Scanner sc = new Scanner(System.in);
-        int opçao1 = sc.nextInt();
-        System.out.println(opçao1);
+        int opçao = 0;
+        do {
+            try {
+                System.out.println("Digite o número de acordo com sua escolha:\n");
+                System.out.println("1 - Modo Aluno\n2 - Modo Disciplina\n3 - Modo Acompanhamento\n");
+                opçao = sc.nextInt();
+                System.out.print("\n");
+                switch (opçao) {
+                    case 1:
+                        System.out.println("---------------------------------------\nVocê escolheu a opção Modo Aluno.\n---------------------------------------\n");
+                        break;
+                    case 2:
+                        System.out.println("---------------------------------------\nVocê escolheu a opção Modo Disciplina.\n---------------------------------------\n");
+                        break;
+                    case 3:
+                        System.out.println("---------------------------------------\nVocê escolheu a opção Modo Acompanhamento.\n---------------------------------------\n");
+                        break;
+                    default:
+                        System.out.println("---------------------------------------\nEssa opçãp não existe! Tente novamente.\n---------------------------------------\n");
+                        break;
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("---------------------------------------\nEntrada inválida! Por favor, digite um número.\n---------------------------------------\n");
+                sc.nextLine();
+            }
+
+        } while (opçao < 1 || opçao > 3);
 
     }
 }
