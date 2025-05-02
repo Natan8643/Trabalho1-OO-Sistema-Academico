622b9236752896c4022f41f81a69f140226129a0
Natan França <natan.j.franca@gmail.com>
2025-05-02 12:04:46 -0300

    implementando a entrada do usuario no menu

--- src/App.java
+++ src/App.java
@@ -1,7 +1,17 @@
+
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
+        System.out.println("\n---------------------------------------");
+        System.out.println("Olá usuário! O que gostaria de fazer?");
         System.out.println("---------------------------------------");
-        System.out.println("Olá usuário!, O que gostaria de fazer?");
-        System.out.println("---------------------------------------");
+        System.out.println("Digite o número de acordo com sua escolha:\n");
+        System.out.println("1 - Modo Aluno\n2 - Modo Disciplina\n3 - Modo Acompanhamento\n");
+
+        Scanner sc = new Scanner(System.in);
+        int opçao1 = sc.nextInt();
+        System.out.println(opçao1);
+
     }
 }
