102ba7c4198c8cc52df719f5278cc6e05afbc4cc
Natan França <natan.j.franca@gmail.com>
2025-05-18 19:57:08 -0300

    cria a funçao cadastrarDisciplina e com seus tratamentos

--- src/DisciplinaManager.java
+++ src/DisciplinaManager.java
@@ -1,7 +1,64 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+
 public class DisciplinaManager {
-    private Disciplina disciplina;
+
+    private List<Disciplina> listaDisciplinas = new ArrayList<>();
+    private final String nomeArquivo = "data/disciplina.csv";
+    private final File arquivo = new File(nomeArquivo);
+    private Menu menu;
+    
+    public void setMenu(Menu menu) {
+        this.menu = menu;
+    }
 
     public void cadastrarDisciplina() {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("\nDigite o nome da disciplina:\n");
+            String nome = sc.nextLine();
+            
+            Integer codigo = null;
+            while (codigo == null) { 
+                try {
+                    System.out.println("\nDigite o código:\n");
+                    codigo = sc.nextInt();
+                    sc.nextLine();
+                    
+                } catch (InputMismatchException e) {
+                    System.out.println("---------------------------------------");
+                    System.out.println("Entrada inválida! Por favor, digite um número.");
+                    System.out.println("---------------------------------------");
+                    sc.nextLine();
+                }
+            }
 
+            Integer cargaHoraria = null;
+            while (cargaHoraria == null) {
+                try {
+                    System.out.println("\nDigite a carga horária (apenas o número em horas)\n");
+                    cargaHoraria = sc.nextInt();
+                    sc.nextLine();
+
+                } catch (InputMismatchException e) {
+                    System.out.println("---------------------------------------");
+                    System.out.println("Entrada inválida! Por favor, digite um número.");
+                    System.out.println("---------------------------------------");
+                    sc.nextLine();
+                }
+            }
+
+
+            System.out.println("\nDigite o pré-requisito:\n");
+            String preRequisito = sc.nextLine();
+
+            Disciplina disciplina = new Disciplina(nome, codigo, cargaHoraria, preRequisito);
+            listaDisciplinas.add(disciplina);
+
+            System.out.println("\nDisciplina cadastrada com sucesso!\n");
+            menu.menuDisciplina();
+        }
     }
 }
