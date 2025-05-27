5926d9755c756bf7dd98b91dc0b072cfa69814ed
Natan França <natan.j.franca@gmail.com>
2025-05-26 22:23:42 -0300

    adiciona verificação de código duplicado ao cadastrar disciplina e implementa listagem de turmas

--- src/DisciplinaManager.java
+++ src/DisciplinaManager.java
@@ -22,7 +22,7 @@ public class DisciplinaManager {
         this.arquivo = new File(nomeArquivo);
         this.menu = null;
     }
-    
+
     public DisciplinaManager(Menu menu) {
         this.listaDisciplinas = new ArrayList<>();
         this.nomeArquivo = "data/disciplina.csv";
@@ -46,6 +46,13 @@ public class DisciplinaManager {
                     System.out.println("\nDigite o código:\n");
                     codigo = sc.nextInt();
                     sc.nextLine();
+
+                    if (buscarDisiciplinaPorCodigo(codigo) != null) {
+                        System.out.println("---------------------------------------");
+                        System.out.println("Já existe uma disciplina com esse código!");
+                        System.out.println("---------------------------------------");
+                        codigo = null;
+                    }
 
                 } catch (InputMismatchException e) {
                     System.out.println("---------------------------------------");
@@ -147,7 +154,6 @@ public class DisciplinaManager {
         menu.menuDisciplina();
     }
 
-
     public Disciplina buscarDisiciplinaPorCodigo(Integer codigo) {
         for (Disciplina disciplina : listaDisciplinas) {
             if (disciplina.getCodigo().equals(codigo)) {
@@ -156,4 +162,40 @@ public class DisciplinaManager {
         }
         return null;
     }
+
+    public void listarTurmas() {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("\nDigite o número da disciplina que deseja listar as turmas:\n");
+            int codigo = sc.nextInt();
+            sc.nextLine();
+
+            Disciplina disciplina = buscarDisiciplinaPorCodigo(codigo);
+            if (disciplina == null) {
+                System.out.println("\nDisciplina não encontrada\n");
+                menu.menuDisciplina();
+            }
+            
+            List<Turma> turmas = disciplina.getTurmas();
+
+            if (turmas.isEmpty()) {
+                System.out.println("\nNenhuma turma cadastrada para essa disciplina\n");
+                menu.menuAluno();
+            }
+
+            System.out.println("\nTurmas cadastradas na disciplina " + disciplina.getNome() + "\n");
+
+            for (Turma turma : turmas) {
+                System.out.println("- Turma: " + turma.getNumeroDaTurma() + " | - Professor: " + turma.getProfessor()
+                        + " | Semestre: " + turma.getSemestre()
+                        + " | Sala: " + turma.getSala()
+                        + " | Horário: " + turma.getHorario()
+                        + "h | Tipo de Aula: " + turma.getTipoDeAula()
+                        + " | Capacidade Máxima: " + turma.getCapacidadeMax()
+                        + " | Número de vagas: " + turma.getVagas() + "\n");
+
+            }
+            menu.menuAluno();
+        }
+    }
+
 }
